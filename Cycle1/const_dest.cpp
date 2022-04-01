507726323a75a8cd22a3544ec15ab09ad663019d
Fayaz Rahman <fayazrahman4u@pg.cusat.ac.in>
2022-04-01 14:50:54 +0530

    update

--- Cycle1/const_dest.cpp
+++ Cycle1/const_dest.cpp
@@ -1,47 +1,90 @@
 #include <iostream>
 
-class Vector
+int count = 0;
+
+class Matrix
 {
-	static int num;
-	int *arr;
-	int len;
-	int obj_num;
+	int **p;
+	int nrows, ncols;
+	int id;
+
 public:
-	Vector(int n){len = n; arr = new int[n]; obj_num = ++num; std::cout << "Vector " << obj_num << " created!\n";}
-	~Vector(){delete arr; std::cout << "Vector " << obj_num << " destructed!\n";}
-	void set_vals();
+	Matrix(){id = ++count; std::cout << "Matrix " << id << "created!.\n";};
+	Matrix(int, int);
+	~Matrix();
+	void alloc_matrix(int, int);
+	void init_matrix();
+	void print_matrix();
 };
 
-int Vector::num = 0;
-
-void Vector::set_vals()
+Matrix::Matrix(int x, int y)
 {
-	std::cout << "Enter elements of vector (seperated by space): ";
-	for (int i = 0; i < len; i++)
+	nrows = x; ncols = y;
+	alloc_matrix(x, y);
+	init_matrix();
+	id = ++count;
+	std::cout << "Matrix " << id << " created!\n\n";
+}
+
+void Matrix::alloc_matrix(int x, int y)
+{
+	p = new int*[nrows];
+
+	for (int i = 0; i < nrows; i++)
+		p[i] = new int[ncols];
+}
+
+void Matrix::init_matrix()
+{
+	std::cout << "Enter elements of matrix: \n";
+	for (int i = 0; i < nrows; i++)
+		for (int j = 0; j < ncols; j++)
+			std::cin >> p[i][j];
+	std::cout << '\n';
+}
+
+Matrix::~Matrix()
+{
+	for (int i = 0; i < nrows; i++)
+		delete[] p[i];
+	delete[] p;
+	std::cout << "Matrix " << id << " deleted!\n\n";
+}
+
+void Matrix::print_matrix()
+{
+	for (int i = 0; i < nrows; i++)
 	{
-		std::cin >> arr[i];
+		for (int  j = 0; j < ncols; j++)
+			std::cout << p[i][j] << ' ';
+		std::cout << '\n';
 	}
 }
 
 int main()
 {
-	int ch = 0;
-	int N;
+	int ch = 0, ch2 = 0, nrows, ncols;
+	Matrix *mtx;
 	do
 	{
-		std::cout << "1.Create vector\n2.END\n>>> ";
+		std::cout << "\n1.Create Matrix\n2.End\n>>> ";
 		std::cin >> ch;
-		
+		std::cout << '\n';
 		if (ch == 1)
 		{
-			std::cout << "Enter size of vector: ";
-			std:: cin >> N;
-			Vector v1(N);
-			v1.set_vals();
-		}
-		else if (ch != 2)
-		{
-			std::cout << "Invalid choice!\n";
+			std::cout << "Enter shape of matrix: ";
+			std::cin >> nrows >> ncols;
+			mtx = new Matrix(nrows, ncols);
+			do
+			{
+				std::cout << "1.Show matrix\n2.Release matrix\n>>> ";
+				std::cin >> ch2;
+				std::cout << '\n';
+				if (ch2 == 1)
+					mtx->print_matrix();
+				else
+					delete mtx;
+			} while (ch2 == 1);
 		}
 	} while (ch != 2);
 }
